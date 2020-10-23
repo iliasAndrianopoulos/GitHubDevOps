b7ef0ff3c146fa97b8dad84ff32e76c26bfe1d39
unknown <kanaxbusiness@gmail.com>
2020-10-23 13:19:30 +0300

    Test commit #8

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace ContosoUniversity
 {
@@ -37,6 +38,13 @@ namespace ContosoUniversity
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
